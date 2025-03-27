2e2a382958bfdaea84c8e272260cc07359e95dcc
Matizin46 <mattheus.paes.br@gmail.com>
2025-03-26 22:22:42 -0300

    Começando JS

--- app.js
+++ app.js
@@ -1,12 +1,59 @@
-//alert("olá, seja bem vindo")
-console.log("posso escrever aqui também")
-console.log(10+2)
-console.log(10-2)
-console.log(10*2)
-console.log(10/2)
-console.log("10"+2)
-console.log("10"*2)
-console.log("João " + "Paulo")
-alert(10+2)
-console.log(parseFloat("10")+2) 
-//converção para numero decimal
+// //alert("olá, seja bem vindo")
+// console.log("posso escrever aqui também")
+// console.log(10+2)
+// console.log(10-2)
+// console.log(10*2)
+// console.log(10/2)
+// console.log("10"+2)
+// console.log("10"*2)
+// console.log("João " + "Paulo")
+// alert(10+2)
+// console.log(parseFloat("10")+2) 
+// //converção para numero decimal
+
+console.log("Matheus")
+console.log(18)
+console.warn("atenção")
+console.error("Um erro")
+
+// var
+// let        variaveis
+// const
+
+let nome = "Matheus" /*Declarando a variavel*/
+let idade = 18
+let telefone = null
+
+console.log (nome, idade) /*Cjama a variavel*/
+nome = "Matheus Paes"
+console.log(nome)
+
+let nomeCompleto = ""
+nomeCompleto = prompt ("Informe seu nome completo")
+console.log(nomeCompleto)
+
+console.log(nomeCompleto)
+// alert("Seja bem vindo(a) " + nomeCompleto)
+// document.write("Seja bem vindo(a) " + nomeCompleto)
+
+
+let cor = prompt ("Informe uma cor: \n 1 azul \n 2 vermelho \n 3 preto")
+let corDeFundo = null
+
+if(cor == "1"){
+    corDeFundo = "blue"
+}else if(cor == "2"){
+    corDeFundo = "red"
+}else if(cor == "3"){
+    corDeFundo = "black"
+}else if(cor == "4"){
+    corDeFundo = "tomato"
+}else if(cor == "5"){
+    corDeFundo = "pink"
+}else if(cor == "6"){
+    corDeFundo = "orange"
+}else{
+    corDeFundo = "yellow"
+}
+
+document.body.style.backgroundColor = corDeFundo
