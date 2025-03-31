06f6e9b3fbe91e1a39b7ee878b6766e763dc5a48
Matizin46 <mattheus.paes.br@gmail.com>
2025-03-31 19:16:59 -0300

    Refatoração de Código

--- app.js
+++ app.js
@@ -1,41 +1,5 @@
-// //alert("olá, seja bem vindo")
-// console.log("posso escrever aqui também")
-// console.log(10+2)
-// console.log(10-2)
-// console.log(10*2)
-// console.log(10/2)
-// console.log("10"+2)
-// console.log("10"*2)
-// console.log("João " + "Paulo")
-// alert(10+2)
-// console.log(parseFloat("10")+2) 
-// //converção para numero decimal
-
-console.log("Matheus")
-console.log(18)
-console.warn("atenção")
-console.error("Um erro")
-
-// var
-// let        variaveis
-// const
-
-let nome = "Matheus" /*Declarando a variavel*/
-let idade = 18
-let telefone = null
-
-console.log (nome, idade) /*Cjama a variavel*/
-nome = "Matheus Paes"
-console.log(nome)
-
 let nomeCompleto = ""
 nomeCompleto = prompt ("Informe seu nome completo")
-console.log(nomeCompleto)
-
-console.log(nomeCompleto)
-// alert("Seja bem vindo(a) " + nomeCompleto)
-// document.write("Seja bem vindo(a) " + nomeCompleto)
-
 
 let cor = prompt ("Informe uma cor: \n 1 azul \n 2 vermelho \n 3 preto \n 4 Tomato \n 5 Rosa \n 6 Laranja")
 let corDeFundo = null
