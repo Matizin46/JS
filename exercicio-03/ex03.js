1353b59dd3eca4d9e43bc3f8257ffaa24750edd9
Matizin46 <mattheus.paes.br@gmail.com>
2025-03-31 21:20:56 -0300

    Exercício03

---
+++ exercicio-03/ex03.js
@@ -0,0 +1,8 @@
+let nome =  prompt("Digite o seu nome:");
+let idade =  prompt("Digite sua idade:");
+
+if(idade < 18){
+    alert("Olá " + nome + " você ainda não pode fazer aulas de direção")
+}else if (idade >=18){
+    alert("Olá " + nome + " , Seja bem vindo(a) a nossa escola! Já fez sua matrícula?")
+}
