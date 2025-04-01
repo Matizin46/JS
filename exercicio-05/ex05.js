dfa21a634de2af90b5c6ae8e66f12cdbafdcc8f7
Matizin46 <mattheus.paes.br@gmail.com>
2025-03-31 21:58:29 -0300

    Exercício05

---
+++ exercicio-05/ex05.js
@@ -0,0 +1,3 @@
+let celsius = parseFloat(prompt("Digite a temperatura em Celsius:"));
+const fahrenheit = (celsius * 9/5) + 32;
+alert("A temperatura equivale a " + fahrenheit + "°F. Fique atento ao clima! ☀️❄️");
